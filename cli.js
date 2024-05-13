a1c9ec3d102e97e28d19c4cc91d7358326789364
Austin Sullivan <ausulliv@redhat.com>
2024-05-13 12:05:19 -0400

    Fix various bugs causing crashes, improve logging for future debugging

--- cli.js
+++ cli.js
@@ -11,12 +11,14 @@ program
   .arguments("<version>", "Which PF versions to use, e.g. alpha, alpha-2, beta")
   .option(
     "-i, --ignore <files>",
-    "Comma-delineated list of files to ignore, files should include their path relative to where this utility is being called. The node_modules dir is ignored by default."
+    "Comma-delineated list of files to ignore, files should include their path relative to where this utility is being called. The node_modules dir is ignored by default.",
+    ""
   )
   .option(
     "-p, --preview",
     "Run this utility in preview mode to see which files will be changed without actually updating them."
   )
+  .option("-v, --verbose", "Include additional logging about processes, mostly useful for debugging.")
   .action(main);
 
 program.parse();
--- index.js
+++ index.js
@@ -2,44 +2,74 @@ const { glob } = require("glob");
 const { writeFile } = require("fs").promises;
 const { join } = require("path");
 
-async function updateVersions(filePath, versions, preview) {
+async function updateVersions(filePath, versions, preview, verbose) {
+  verbose && console.log("Inspecting", filePath);
+
   const fileData = require(join(process.cwd(), filePath));
-  const { dependencies, devDependencies, peerDependencies } = fileData;
   const packagesInRelease = Object.keys(versions);
 
+  verbose && console.log("PF packages in this release: ", packagesInRelease);
+
+  let updates = {};
+
   packagesInRelease.forEach((package) => {
-    if (dependencies[package]) {
-      dependencies[package] = versions[package];
-    }
+    ["dependencies", "devDependencies", "peerDependencies"].forEach(
+      (depKind) => {
+        const depValue = fileData[depKind];
+        if (!depValue) {
+          return;
+        }
 
-    if (devDependencies[package]) {
-      devDependencies[package] = versions[package];
-    }
+        const currentPackageValue = depValue[package];
+        const newPackageValue = versions[package];
 
-    if (peerDependencies[package]) {
-      peerDependencies[package] = versions[package];
-    }
+        if (!currentPackageValue || currentPackageValue === newPackageValue) {
+          return;
+        }
+
+        fileData[depKind][package] = newPackageValue;
+        updates = {
+          ...updates,
+          [depKind]: {
+            ...updates[depKind],
+            [package]: { current: currentPackageValue, new: newPackageValue },
+          },
+        };
+      }
+    );
   });
 
+  console.log(filePath, ": ", updates);
+
   if (preview) {
-    console.log("Will update the file ", filePath, " to now be ", fileData);
-  } else {
+    return;
+  }
+
+  if (Object.keys(updates).length) {
     await writeFile(filePath, JSON.stringify(fileData));
   }
 }
 
 async function main(version, options) {
-  const { ignore, preview } = options;
+  const { ignore, preview, verbose } = options;
 
   const packageFiles = await glob("**/package.json", {
     ignore: [...ignore.split(","), "node_modules/**"],
   });
 
+  verbose && console.log("Found files: ", packageFiles);
+
   const versions = require("./versions.json")[version];
 
   packageFiles.forEach((packageFile) =>
-    updateVersions(packageFile, versions, preview)
+    updateVersions(packageFile, versions, preview, verbose)
   );
+
+  if (preview) {
+    console.log(
+      "Exiting without making changes because this is running in preview mode."
+    );
+  }
 }
 
 module.exports = { main };
